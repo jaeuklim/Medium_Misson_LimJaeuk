28d54195ed5051577c172faa0a1755dc9e2e5de7
jaeuklim <jeuk007@skuniv.ac.kr>
2023-12-12 16:30:21 +0900

    misson 1

---
+++ src/main/java/com/ll/medium/domain/user/SiteUser.java
@@ -0,0 +1,20 @@
+package com.ll.medium.domain.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class SiteUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+}
